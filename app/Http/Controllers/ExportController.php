0a38e5f7d9b385f010ee9ae33d227c4ec66f3917
Khaled Kamr <200013171@must.edu.eg>
2025-11-16 15:21:18 +0200

    fix show container route

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -127,7 +127,7 @@ class ExportController extends Controller
             $container->storage_price = $container->policies->where('type', 'تخزين')->first()->storage_price;
             if($container->period > $container->policies->where('type', 'تخزين')->first()->storage_duration) {
                 $days = (int) Carbon::parse($container->date)
-                    ->addDays($container->policies->where('type', 'تخزين')->first()->storage_duration)
+                    ->addDays((int) $container->policies->where('type', 'تخزين')->first()->storage_duration)
                     ->diffInDays(Carbon::parse($container->exit_date));
                 $container->late_days = $days;
                 $container->late_fee = $days * $container->policies->where('type', 'تخزين')->first()->late_fee;
