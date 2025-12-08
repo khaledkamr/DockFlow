757bbc662e360de03af043fac006149645bb3fc7
Khaled Kamr <200013171@must.edu.eg>
2025-12-08 21:39:41 +0200

    add comments to trait

--- app/Traits/BelongsToCompany.php
+++ app/Traits/BelongsToCompany.php
@@ -6,6 +6,22 @@ use Illuminate\Database\Eloquent\Builder;
 
 trait BelongsToCompany
 {
+    /**
+     * This trait uses SESSION (not Auth::user()) to get company_id.
+     *
+     * Because using Auth::user() inside a global scope causes a disaster:
+     * - The User model gets loaded.
+     * - The trait re-applies its global scope during User load.
+     * - That scope again tries to call Auth::user().
+     * - Infinite loop.
+     * - Whole system dies. Pages break. Life collapses.
+     *
+     * Using session('company_id') avoids all of that.
+     *
+     * If you ever think "let me change it to Auth::user()"...
+     * DON'T.
+     * This comment exists so Future-Me doesn't forget and burn the app again.
+     */
     protected static function bootBelongsToCompany()
     {
         static::addGlobalScope('company', function (Builder $builder) {
