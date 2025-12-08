88d7ffa3cbc5b4aa07bc4908e8e0254aa6fb33ba
Khaled Kamr <200013171@must.edu.eg>
2025-12-08 19:58:04 +0200

    fix belongsToCompany trait

--- app/Traits/BelongsToCompany.php
+++ app/Traits/BelongsToCompany.php
@@ -3,21 +3,20 @@
 namespace App\Traits;
 
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Support\Facades\Auth;
 
 trait BelongsToCompany
 {
     protected static function bootBelongsToCompany()
     {
         static::addGlobalScope('company', function (Builder $builder) {
-            if (Auth::check() && Auth::user()->company_id) {
-                $builder->where('company_id', Auth::user()->company_id);
+            if (session()->has('company_id')) {
+                $builder->where('company_id', session('company_id'));
             }
         });
 
         static::creating(function ($model) {
-            if (Auth::check() && Auth::user()->company_id && !$model->company_id) {
-                $model->company_id = Auth::user()->company_id;
+            if (session()->has('company_id')) {
+                $model->company_id = session('company_id');
             }
         });
     }
