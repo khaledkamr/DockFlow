84f8a7d37f850a785e8f38cbc43c62c61dfdd70a
Khaled Kamr <200013171@must.edu.eg>
2026-03-15 13:12:06 +0200

    fix calculating total for account statement balance

--- resources/views/pages/accounting/reports/account_statement.blade.php
+++ resources/views/pages/accounting/reports/account_statement.blade.php
@@ -116,7 +116,7 @@
                         <td colspan="5" class="text-center fs-6">الإجماليـــــات</td>
                         <td class="text-center">{{ number_format($statement->sum(fn($line) => $line->debit) + ($opening_balance > 0 ? $opening_balance : 0), 2) }}</td>
                         <td class="text-center">{{ number_format($statement->sum(fn($line) => $line->credit) + ($opening_balance < 0 ? abs($opening_balance) : 0), 2) }}</td>
-                        <td class="text-center">{{ number_format($balance + $opening_balance, 2) }}</td>
+                        <td class="text-center">{{ number_format($balance, 2) }}</td>
                     </tr>
                 @else
                     <tr>
