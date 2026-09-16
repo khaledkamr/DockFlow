5fd1eb53623e59ccd6a159da2c8eeea70c71810f
Khaled Kamr <200013171@must.edu.eg>
2026-09-16 22:41:59 +0300

    fix: format profit values to two decimal places in dashboard calculations

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -129,7 +129,7 @@ class DashboardController extends Controller
                 'month' => $from->format('F'),
                 'revenues' => $revenue,
                 'expenses' => $expenses,
-                'profit' => $revenue - $expenses,
+                'profit' => number_format($revenue - $expenses, 2),
             ];
         }
 
@@ -237,7 +237,7 @@ class DashboardController extends Controller
                 'month' => $from->format('F'),
                 'revenues' => $revenue,
                 'expenses' => $expenses,
-                'profit' => $revenue - $expenses,
+                'profit' => number_format($revenue - $expenses, 2),
             ];
         }
 
